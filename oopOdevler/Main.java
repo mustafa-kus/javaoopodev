450d4fef806ab0f5b6722e11c23bffdbf85b7c25
mustafa kuş <mustafa_kus@hotmail.com>
2021-04-25 22:35:34 +0300

    a

--- oopOdevler/Main.java
+++ oopOdevler/Main.java
@@ -1,9 +1,14 @@
 package oopOdevler;
 
+import sun.security.util.Event.ReporterCategory;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		CategoryManager categoryManager=new CategoryManager();
+		categoryManager.Add(1,"Commercial");
+		categoryManager.List();
 		CourseManager courseManager=new CourseManager();
 		courseManager.Add(1, 1, "Java", "Engin", 0);
 		courseManager.List();
